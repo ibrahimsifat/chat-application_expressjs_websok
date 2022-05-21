164cebe13974dc7c31aba3a5dc5febec2f018bf4
ibrahimsifat <ibsifat24@gmail.com>
2022-05-21 17:15:34 +0300

    ADD USER

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,5 +1,47 @@
+// external export
+const bcrypt = require("bcrypt");
+const { unlink } = require("fs");
+const path = require("path");
+
+// internal export
+const User = require("../models/People");
+
 // to get users page
 function getUsers(req, res) {
   res.render("users");
 }
-module.exports = { getUsers };
+
+// add user
+async function addUser(req, res, next) {
+  let newUser;
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  if (req.files && req.files.length > 0) {
+    newUser = new User({
+      ...req.body,
+      avatar: req.files[0].filename,
+      password: hashedPassword,
+    });
+  } else {
+    newUser = new User({
+      ...req.body,
+      password: hashedPassword,
+    });
+  }
+
+  // save user on send error
+  try {
+    const result = await newUser.save();
+    res.status(200).json({
+      massage: "user was added successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: {
+        common: {
+          msg: "unknown error occured!",
+        },
+      },
+    });
+  }
+}
+module.exports = { getUsers, addUser };
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -2,10 +2,21 @@
 const express = require("express");
 
 // internal import
-const { getUsers } = require("../controller/usersController");
+const { getUsers, addUser } = require("../controller/usersController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
-
+const avatarUpload = require("../middlewares/user/avatarUpload");
+const {
+  addUserValidators,
+  addUserValidationHandler,
+} = require("../middlewares/user/userValidators");
 const router = express.Router();
 
 router.get("/", decorateHtmlResponse("Users"), getUsers);
+router.post(
+  "/",
+  avatarUpload,
+  addUserValidators,
+  addUserValidationHandler,
+  addUser
+);
 module.exports = router;
