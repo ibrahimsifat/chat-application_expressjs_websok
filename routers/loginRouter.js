c395eb3248e4726a9c5912f1cb519bb87a1cf3af
ibrahimsifat <ibsifat24@gmail.com>
2022-05-21 21:45:57 +0300

    LoginLogout

--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -2,10 +2,21 @@
 const express = require("express");
 
 // internal import
-const { getLogin } = require("../controller/loginController");
+const { getLogin, login, logout } = require("../controller/loginController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
-
+const {
+  doLoginValidators,
+  doLoginValidationHandler,
+} = require("../middlewares/login/loginValidators");
 const router = express.Router();
-
-router.get("/", decorateHtmlResponse("Login"), getLogin);
+const page_title = "Login";
+router.get("/", decorateHtmlResponse(page_title), getLogin);
+router.post(
+  "/",
+  decorateHtmlResponse(page_title),
+  doLoginValidators,
+  doLoginValidationHandler,
+  login
+);
+router.delete("/", logout);
 module.exports = router;
